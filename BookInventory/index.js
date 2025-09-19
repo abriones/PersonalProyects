f5c15fc57dbc4250a039c29f199a6d4ed938b563
Andres Briones <thestranger117@gmail.com>
2025-09-19 14:48:56 -0600

    Making a book inventory tracking app!

---
+++ BookInventory/index.js
@@ -0,0 +1,48 @@
+const port = 3000;
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const axios = require('axios');
+const engine = require('ejs-mate');
+
+const app = express();
+
+const usersRoute = require('./routes/users');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.engine('ejs',engine)
+app.use('/',usersRoute)
+
+
+app.get('/', (req, res) => {
+    res.redirect('home')
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+})
+
+app.get('/shelf', (req, res) => {
+    res.render('shelf');
+})
+
+app.get('/results',async (req, res) => {
+    try {
+        const userSearch = req.query.searchBar
+        const config = { params: { q: userSearch } }
+        const response = await axios.get('https://openlibrary.org/search.json?',config);
+        const pass = response.data
+        res.render('results', {pass,userSearch})
+        
+    } catch (error) {
+        console.error('Error fetching external data:', error);
+        res.status(500).json({ message: 'Error fetching data from external API' });
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log(`Listening on port ${port}`)
+});
